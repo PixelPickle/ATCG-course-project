8b22443c30454f7ae40282208dd91f2e672af1b4
Kaylen Bates <ktbates@basspro.com>
2019-07-25 09:18:57 -0500

    Up To: 284

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -9,37 +9,43 @@ export class RecipeService {
 
   recipesChanged = new Subject<Recipe[]>();
 
-  private recipes: Recipe[] = [
-    new Recipe(
-      'Test Name A',
-      'Test Description A',
-      'https://upload.wikimedia.org/wikipedia/commons/5/57/Chicken_fettuccine_alfredo.JPG',
-      [
-        new Ingredient('Chicken', 1),
-        new Ingredient('Fettuccine', 20)
-      ] ),
-    new Recipe(
-      'Test Name B',
-      'Test Description B',
-      'https://upload.wikimedia.org/wikipedia/commons/5/57/Chicken_fettuccine_alfredo.JPG',
-      [
-        new Ingredient('Duck', 2),
-        new Ingredient('Mancini', 40)
-      ] ),
-    new Recipe(
-      'Test Name C',
-      'Test Description C',
-      'https://upload.wikimedia.org/wikipedia/commons/5/57/Chicken_fettuccine_alfredo.JPG',
-      [
-        new Ingredient('Steak', 3),
-        new Ingredient('Broccoli', 4)
-      ])
-  ];
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     'Test Name A',
+  //     'Test Description A',
+  //     'https://upload.wikimedia.org/wikipedia/commons/5/57/Chicken_fettuccine_alfredo.JPG',
+  //     [
+  //       new Ingredient('Chicken', 1),
+  //       new Ingredient('Fettuccine', 20)
+  //     ] ),
+  //   new Recipe(
+  //     'Test Name B',
+  //     'Test Description B',
+  //     'https://upload.wikimedia.org/wikipedia/commons/5/57/Chicken_fettuccine_alfredo.JPG',
+  //     [
+  //       new Ingredient('Duck', 2),
+  //       new Ingredient('Mancini', 40)
+  //     ] ),
+  //   new Recipe(
+  //     'Test Name C',
+  //     'Test Description C',
+  //     'https://upload.wikimedia.org/wikipedia/commons/5/57/Chicken_fettuccine_alfredo.JPG',
+  //     [
+  //       new Ingredient('Steak', 3),
+  //       new Ingredient('Broccoli', 4)
+  //     ])
+  // ];
+  private recipes: Recipe[] = [];
 
   constructor( private shoppingListService: ShoppingListService ) {}
 
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  setRecipes( recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice());
   }
 
   getRecipeByID( id: number ) {
