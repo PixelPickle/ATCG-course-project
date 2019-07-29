3c45b37ac5721275ae300fd0c5972aa81e2ca840
Kaylen Bates <ktbates@basspro.com>
2019-07-29 16:26:05 -0500

    Up to 330

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,16 @@
+import {NgModule} from '@angular/core';
+import {SharedModule} from '../shared/shared.module';
+import {AuthComponent} from './auth.component';
+import {RouterModule} from '@angular/router';
+
+@NgModule({
+  declarations: [
+    AuthComponent
+  ],
+  imports: [
+    SharedModule,
+    RouterModule.forChild([{ path: 'auth', component: AuthComponent }])
+  ],
+  exports: []
+})
+export class AuthModule { }
